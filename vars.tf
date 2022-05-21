18ecef89b33bb0f6e200caf94e6698aed95600d6
Bruno Higino de Souza <72587932+BrunoHigino06@users.noreply.github.com>
2022-05-21 05:36:11 -0300

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "subnet_name_association" {
+  type = list(string)
+  description = "Names of the subnets to associate with a route table"
+}
+
+variable "route_table_name_association" {
+  type = list(string)
+  description = "name of the route tables to association with the subnets"
+}
